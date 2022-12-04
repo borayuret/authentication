754a31c901eeb80eaa97a04994d128d8c156b689
Bora Yuret <borayuret@gmail.com>
2022-12-04 19:31:53 +0300

    added jasmine conf

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-console.log('Hello World :) ')
+import express from 'express';
 
-let number;
+// create instance server
+const app = express();
+
+const PORT = 3000;
+
+// add routing for path /
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
+
+// start express server
+app.listen(PORT, () => {
+    console.log(`App runs on port ${PORT}`)
+})
+
+export default app;
